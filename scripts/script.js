4a6014dc2d5c5aef6601ad35d036d8ff1b591bae
MarijnMolenaar <32360693+MarijnMolenaar@users.noreply.github.com>
2021-12-13 17:53:44 +0100

    Update 13-12-2021

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('navUl');
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
